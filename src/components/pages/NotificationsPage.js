6e432774d8b6cace153b1f43d35846dae1ee79c8
Igor <prostopochtaivso@gmail.com>
2022-06-11 03:10:29 +0300

    добавил удаление всех уведомлений

--- src/components/pages/NotificationsPage.js
+++ src/components/pages/NotificationsPage.js
@@ -5,29 +5,37 @@ const NotificationsPage = () => {
    const user = JSON.parse(localStorage.getItem('user'));
    const [newNotifications, setNewNotifications] = useState([]);
    const [oldNotifications, setOldNotifications] = useState([]);
+   const [removeAllNotifications, setRemoveAllNotifications] = useState(false);
 
    useEffect(() => {
+      getNotifications();
+
+      if (user.notifications && user.notifications.new) {
+         transferNotifications();
+      }
+   }, []);
+
+   const getNotifications = () => {
+      const normalUser = JSON.parse(localStorage.getItem('user'));
       const userNewNotifications = [];
       const userOldNotifications = [];
 
-      if (user.notifications) {
-         if (user.notifications.new) {
-            for (let key in user.notifications.new) {
-               userNewNotifications.push(user.notifications.new[key]);
+      if (normalUser.notifications) {
+         if (normalUser.notifications.new) {
+            for (let key in normalUser.notifications.new) {
+               userNewNotifications.push(normalUser.notifications.new[key]);
             }
             setNewNotifications(userNewNotifications);
-
-            transferNotifications();
          }
 
-         if (user.notifications.old) {
-            for (let key in user.notifications.old) {
-               userOldNotifications.push(user.notifications.old[key]);
+         if (normalUser.notifications.old) {
+            for (let key in normalUser.notifications.old) {
+               userOldNotifications.push(normalUser.notifications.old[key]);
             }
             setOldNotifications(userOldNotifications);
          }
       }
-   }, []);
+   };
 
    const transferNotifications = async () => {
       await set(ref(database, `users/${user.name}/notifications/old`), {
@@ -35,7 +43,7 @@ const NotificationsPage = () => {
          ...user.notifications.new,
       }).then(async () => {
          await set(ref(database, `users/${user.name}/notifications/new`), null);
-         localStorage.setItem(
+         await localStorage.setItem(
             'user',
             JSON.stringify({
                ...user,
@@ -50,9 +58,30 @@ const NotificationsPage = () => {
       });
    };
 
+   const clearNotifications = async () => {
+      const userData = JSON.parse(JSON.stringify(user));
+
+      await set(ref(database, `users/${user.name}/notifications`), null).then(
+         () => {
+            delete userData.notifications;
+
+            setRemoveAllNotifications(true);
+
+            setTimeout(() => {
+               localStorage.setItem('user', JSON.stringify(userData));
+               setNewNotifications([]);
+               setOldNotifications([]);
+            }, 400);
+         }
+      );
+   };
+
    return (
       <div className="notifications-page mw1400 animate__animated animate__fadeIn">
          <h2 className="titleh2">Уведомления</h2>
+         <button className="notifacations__clear" onClick={clearNotifications}>
+            Очистить всё
+         </button>
          {user.notifications ? (
             <>
                {newNotifications.length !== 0 ? (
@@ -66,7 +95,14 @@ const NotificationsPage = () => {
                         .sort((a, b) => a.clearDate - b.clearDate)
                         .reverse()
                         .map((item, i) => (
-                           <div className="notification" key={i}>
+                           <div
+                              className={
+                                 removeAllNotifications
+                                    ? 'notification animate__animated animate__zoomOut'
+                                    : 'notification'
+                              }
+                              key={i}
+                           >
                               <div className="notification__date">
                                  Дата: {item.date}
                               </div>
@@ -94,7 +130,14 @@ const NotificationsPage = () => {
                         .sort((a, b) => a.clearDate - b.clearDate)
                         .reverse()
                         .map((item, i) => (
-                           <div className="notification" key={i}>
+                           <div
+                              className={
+                                 removeAllNotifications
+                                    ? 'notification animate__animated animate__zoomOut'
+                                    : 'notification'
+                              }
+                              key={i}
+                           >
                               <div className="notification__date">
                                  Дата: {item.date}
                               </div>
